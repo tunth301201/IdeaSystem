5de9760386f8e870b936ace52ec13bcd5cd7dc42
zocabulapo <zocabulapo@gmail.com>
2023-03-14 21:34:00 +0700

    AddTags

--- routes/tag.route.js
+++ routes/tag.route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const tagController = require('../controllers/tag.controller');
+
+// POST /tags
+router.post('/', tagController.createTag);
 // GET /tags
 router.get('/', tagController.getTags);
 module.exports = router;
