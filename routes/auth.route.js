5087dc91cbbdb6c82a7ab8035bc2ddeb00a96988
zocabulapo <zocabulapo@gmail.com>
2023-03-25 05:26:21 +0700

    Login user

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,5 +1,4 @@
 const router=require('express').Router();
 const authController=require('./../controllers/auth.controller')
-router.post('/register',authController.register);
 router.post('/login',authController.login);
 module.exports=router;
