32070bbff16253cce4e1a4bfc74f96fa25aab75a
nguyenvo811 <nguyenvo709@gmail.com>
2023-04-09 15:46:59 +0700

    Fixed sidenav

--- src/components/NavBar/Navigation.jsx
+++ src/components/NavBar/Navigation.jsx
@@ -13,8 +13,8 @@ export default function Navigation() {
                         </svg>
                     </button>
                     <a href="https://flowbite.com" className="flex ml-2 md:mr-24">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
+                        <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
+                        <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">Flowbite</span>
                     </a>
                 </div>
                 <div className="flex items-center">
